7b2412ce9bf96b408317bd7b3f97c7080863f4fe
Jean-Marie Ferdegue <jmf@fb.com>
2020-11-14 09:57:44 +0000

    Improves logging

--- pages/api/beneficiary/index.js
+++ pages/api/beneficiary/index.js
@@ -12,7 +12,12 @@ export default async function me(req, res) {
   try {
     user = await User.findByPk(session.user.sub);
   } catch(error) {
-    res.status(error.status || 500).end(error.message);
+    if (error.message && error.status) {
+        res.status(error.status || 500).end(error.message);
+    } else {
+        res.status(500).end("Exception thrown during retrieval of user with no message")
+    }
+
     return
   }
   switch(req.method) {
