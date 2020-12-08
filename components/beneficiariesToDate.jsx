0b4db67b8b17f04afe8ad933c429da4a952a7fca
Jean-Marie Ferdegue <jeanmarie.ferdegue@gmail.com>
2020-12-08 21:49:45 +0000

    Only shows donation of a given user

--- components/beneficiariesToDate.jsx
+++ components/beneficiariesToDate.jsx
@@ -78,23 +78,23 @@ class BeneficiariesToDate extends Component {
               <tr>
                 <td>{item.nickname}</td>
                 <td>
-                  {this.props.donation.list.filter(x=>(x.gift.target_beneficiary && x.gift.target_beneficiary.id==item.id)).length == 0 ? (
+                  {this.props.donation.list.filter(x=>x.user.identifiant==this.props.user.identifiant).filter(x=>(x.gift.target_beneficiary && x.gift.target_beneficiary.id==item.id)).length == 0 ? (
                     <>Rien pour l'instant. <a href={"/list?beneficiary="+item.id}>Voir sa liste</a></>
                   ) : (
                     <ul>
-                      {this.props.donation.list.filter(x=>(x.gift.target_beneficiary && x.gift.target_beneficiary.id==item.id)).map(oneDonation=> (
+                      {this.props.donation.list.filter(x=>x.user.identifiant==this.props.user.identifiant).filter(x=>(x.gift.target_beneficiary && x.gift.target_beneficiary.id==item.id)).map(oneDonation=> (
                         <li>{oneDonation.gift.title}{" "}({oneDonation.amount} Euros)</li>
                       ))}
                     </ul>
                   )}
 
                 </td>
-                <td>{this.props.donation.list.filter(x=>(x.gift.target_beneficiary && x.gift.target_beneficiary.id==item.id)).map(x=>x.amount).reduce((a, b) => parseInt(a) + parseInt(b), 0)} Euros</td>
+                <td>{this.props.donation.list.filter(x=>x.user.identifiant==this.props.user.identifiant).filter(x=>(x.gift.target_beneficiary && x.gift.target_beneficiary.id==item.id)).map(x=>x.amount).reduce((a, b) => parseInt(a) + parseInt(b), 0)} Euros</td>
               </tr>
             ))}
             <tr>
               <td colSpan="2">Cout total...</td>
-              <td>{this.props.donation.list.map(x=>x.amount).reduce((a, b) => parseInt(a) + parseInt(b), 0)} Euros</td>
+              <td>{this.props.donation.list.filter(x=>x.user.identifiant==this.props.user.identifiant).map(x=>x.amount).reduce((a, b) => parseInt(a) + parseInt(b), 0)} Euros</td>
             </tr>
 
 
